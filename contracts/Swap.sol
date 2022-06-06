c0d65753a4bfd1d26711c6c0b24e5c62ed4f95ea
Siddhi Aphiphunya <siddhi@hey.com>
2022-06-06 17:47:48 +0000

    ✨✅ [SA] uniswap swapping functionality

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -3,6 +3,27 @@ pragma solidity ^0.8.3;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+interface IUniswapV2Router {
+  function getAmountsOut(uint256 amountIn, address[] memory path)
+    external
+    view
+    returns (uint256[] memory amounts);
+
+  function swapExactTokensForTokens(
+
+    //amount of tokens we are sending in
+    uint256 amountIn,
+    //the minimum amount of tokens we want out of the trade
+    uint256 amountOutMin,
+    //list of token addresses we are going to trade in.  this is necessary to calculate amounts
+    address[] calldata path,
+    //this is the address we are going to send the output tokens to
+    address to,
+    //the last time that the trade is valid for
+    uint256 deadline
+  ) external returns (uint256[] memory amounts);
+}
+
 contract UniswapGovernance {
     using SafeERC20 for IERC20;
 
@@ -10,11 +31,30 @@ contract UniswapGovernance {
     address constant private CVX_CONTRACT = 0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B;
     address constant private CRV3LP_CONTRACT = 0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490;
 
-    function CRVTo3CRVLP() public {
+    IERC20 constant private CRV = IERC20(CRV_CONTRACT);
+    IERC20 constant private CVX = IERC20(CVX_CONTRACT);
+    IERC20 constant private CRV3LP = IERC20(CRV3LP_CONTRACT);
 
+    address private constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function swap(address _tokenIn, address _tokenOut, uint256 _amountIn, uint256 _amountOutMin, address _to) public {
+        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
+
+        address[] memory path;
+    if (_tokenIn == WETH || _tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = _tokenIn;
+      path[1] = _tokenOut;
+    } else {
+      path = new address[](3);
+      path[0] = _tokenIn;
+      path[1] = WETH;
+      path[2] = _tokenOut;
     }
-
-    function CVCTo3CRVLP() public {
-
+        //then we will call swapExactTokensForTokens
+        //for the deadline we will pass in block.timestamp
+        //the deadline is the latest time the trade is valid for
+        IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(_amountIn, _amountOutMin, path, _to, block.timestamp);
     }
 }
--- test/test-swap-governance.js
+++ test/test-swap-governance.js
@@ -7,6 +7,8 @@ const CVX_WHALE = '0xb576491f1e6e5e62f1d8f26062ee822b40b0e0d4';
 const CVX = '0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B';
 const CRV3LP = '0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490';
 
+const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+
 const ERC20_DECIMAL = 18;
 
 contract('TestUniswapGovernance', (accounts) => {
@@ -15,6 +17,7 @@ contract('TestUniswapGovernance', (accounts) => {
         CVX_CONTRACT = await IERC20.at(CVX);
         CRV3LP_CONTRACT = await IERC20.at(CRV3LP);
         SWAP_CONTRACT = await UNISWAP_GOVERNANCE.new();
+        DAI_CONTRACT = await IERC20.at(DAI);
 
         const etherAmount = 1
 
@@ -36,8 +39,6 @@ contract('TestUniswapGovernance', (accounts) => {
         await sendERC20(CRV_CONTRACT, CRV_WHALE, SWAP_CONTRACT.address, unnormalise(erc20Amount, ERC20_DECIMAL));
         await sendERC20(CVX_CONTRACT, CVX_WHALE, SWAP_CONTRACT.address, unnormalise(erc20Amount, ERC20_DECIMAL));
 
-        await debugDisplayAll(accounts[0]);
-
         const contractCRVbal = await CRV_CONTRACT.balanceOf(SWAP_CONTRACT.address);
         const contractCVXbal = await CVX_CONTRACT.balanceOf(SWAP_CONTRACT.address);
         const contract3CRVLPbal = await CRV3LP_CONTRACT.balanceOf(SWAP_CONTRACT.address);
@@ -48,18 +49,35 @@ contract('TestUniswapGovernance', (accounts) => {
     });
 
     it('swapsCRVTo3CRV', async () => {
-        return;
+        const absToSwap = 10;
+
+        const swapAmount = unnormalise(absToSwap, ERC20_DECIMAL);
+        const outputAmount = unnormalise(1, ERC20_DECIMAL);
+
+        const initialContractBalCRV = normalise(await CRV_CONTRACT.balanceOf(SWAP_CONTRACT.address), ERC20_DECIMAL);
+
+        await SWAP_CONTRACT.swap(CRV, DAI, swapAmount, outputAmount, accounts[0]);
+
+        const actualAccountBalDAI = normalise(await DAI_CONTRACT.balanceOf(accounts[0]), ERC20_DECIMAL);
+        const actualContractBalCRV = normalise(await CRV_CONTRACT.balanceOf(SWAP_CONTRACT.address), ERC20_DECIMAL);
+
+        assert.notEqual(actualAccountBalDAI, 0, "There should exist DAI from CRV swap");
+        assert.equal(actualContractBalCRV, initialContractBalCRV - absToSwap, `Only ${swapAmount.toString()} of CRV should have been swapped`);
     });
 
-    // it('swapsCVXTo3CRV', async () => {
-    //     return;
-    // });
+    // TODO Issue with insufficient balance
+    it.skip('swapsCVXTo3CRV', async () => {
+        const swapAmount = unnormalise(1000, ERC20_DECIMAL);
+        const outputAmount = unnormalise(8000, ERC20_DECIMAL);
+        await SWAP_CONTRACT.swap(CVX, DAI, swapAmount, outputAmount, SWAP_CONTRACT.address);
+    });
 });
 
 const debugDisplayAll = async (account) => {
     const accountBalCRV = await CRV_CONTRACT.balanceOf(account);
     const accountBalCVX = await CVX_CONTRACT.balanceOf(account);
     const accountBal3CRV = await CRV3LP_CONTRACT.balanceOf(account);
+    const accountBalDAI = await DAI_CONTRACT.balanceOf(account);
     const contractBalCRV = await CRV_CONTRACT.balanceOf(SWAP_CONTRACT.address);
     const contractBalCVX = await CVX_CONTRACT.balanceOf(SWAP_CONTRACT.address);
     const contractBal3CRVLP = await CRV3LP_CONTRACT.balanceOf(SWAP_CONTRACT.address);
@@ -69,6 +87,7 @@ const debugDisplayAll = async (account) => {
     console.log("Account CRV Bal:", accountBalCRV.toString());
     console.log("Account CVX Bal:", accountBalCVX.toString());
     console.log("Account 3CRV LP Bal:", accountBal3CRV.toString());
+    console.log("Account DAI Bal:", normalise(accountBalDAI.toString(), ERC20_DECIMAL).toString());
 
     console.log("\n");
 
